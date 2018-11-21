2187825f84a38fe437bfc55db147b4f6f11e2789
tbielaszewski <tomekbielaszewski@gmail.com>
2018-11-21 18:40:56 +0100

    pangram java

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -2,11 +2,11 @@ public class PangramChecker {
     private final static int LETTERS_IN_ALPHABET = 26;
 
     public boolean isPangram(String input) {
-        input = input.toLowerCase()
-                .replaceAll(" ", "")
-                .replaceAll("[^a-z]+", "");
-        return input.matches("[a-z]+") &&
-                input.chars().distinct().count() == LETTERS_IN_ALPHABET;
+        return input.toLowerCase()
+                .replaceAll("[^a-z]+", "")
+                .chars()
+                .distinct()
+                .count() == LETTERS_IN_ALPHABET;
     }
 
 }
