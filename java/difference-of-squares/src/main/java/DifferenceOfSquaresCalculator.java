6ee08ab127ddf26bca71e6eb70af904d3d2d34c3
tbielaszewski <tomekbielaszewski@gmail.com>
2019-04-02 20:54:22 +0200

    java:Difference Of Squares - usage of rangeClosed on IntStream to get rid of ++input

--- java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -3,12 +3,12 @@ import java.util.stream.IntStream;
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        return (int) Math.pow(IntStream.range(1, ++input)
+        return (int) Math.pow(IntStream.rangeClosed(1, input)
                 .sum(), 2);
     }
 
     int computeSumOfSquaresTo(int input) {
-        return IntStream.range(1, ++input)
+        return IntStream.rangeClosed(1, input)
                 .map(i -> i * i)
                 .sum();
     }
