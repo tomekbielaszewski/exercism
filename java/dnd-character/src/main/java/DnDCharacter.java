6a963c1817f0fb8698d87a1859303f4653097f6b
tbielaszewski <tbielaszewski@objectivity.co.uk>
2019-04-02 15:02:50 +0200

    java:D&D Character - missing methods implemented - tests compiles

--- java/dnd-character/src/main/java/DnDCharacter.java
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -1,4 +1,11 @@
 class DnDCharacter {
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+    private int hitpoints;
 
     int ability() {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
@@ -8,4 +15,31 @@ class DnDCharacter {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
+    int getStrength() {
+        return strength;
+    }
+
+    int getDexterity() {
+        return dexterity;
+    }
+
+    int getConstitution() {
+        return constitution;
+    }
+
+    int getIntelligence() {
+        return intelligence;
+    }
+
+    int getWisdom() {
+        return wisdom;
+    }
+
+    int getCharisma() {
+        return charisma;
+    }
+
+    int getHitpoints() {
+        return hitpoints;
+    }
 }
