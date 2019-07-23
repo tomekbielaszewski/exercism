10bbe3841433b2ce9ec66ab3724d801b0e1cc060
tbielaszewski <tomekbielaszewski@gmail.com>
2019-07-23 16:14:53 +0200

    sum of multiples - solution

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,32 @@
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class SumOfMultiples {
+    private final int number;
+    private final int[] set;
 
     SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.number = number;
+        this.set = set;
     }
 
     int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Arrays.stream(set)
+                .filter(this::nonZero)
+                .mapToObj(divider -> IntStream.range(0, number)
+                        .filter(i -> i % divider == 0)
+                        .boxed()
+                )
+                .flatMap(Function.identity())
+                .collect(Collectors.toSet())
+                .stream()
+                .reduce(0, Integer::sum);
+    }
+
+    private boolean nonZero(int i) {
+        return i != 0;
     }
 
 }
