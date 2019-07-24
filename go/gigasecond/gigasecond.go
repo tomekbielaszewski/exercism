343b0c4b880953e4ffda36c0d4448b8f5d4985d4
tbielaszewski <tomekbielaszewski@gmail.com>
2019-07-24 16:13:55 +0200

    golang: gigasecond solution with int literal

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -2,9 +2,9 @@ package gigasecond
 
 import "time"
 
-const GIGASECOND time.Duration = 1000000000 * time.Second
+const GIGASECOND = 1e9 * time.Second
 
-// Returns a moment whe one lived for 1'000'000'000 seconds
+// Returns a moment when one lived for 1'000'000'000 seconds
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(time.Duration(GIGASECOND))
 }
